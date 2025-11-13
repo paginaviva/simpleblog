e9f245e0e5728c3c7f31842b8b8ec1f003a66d15
PáginaVIVA <tools@paginaviva.net>
2025-11-13 12:02:16 +0000

    Actualiza rama con el contenido del workspace

--- js/gtag.js
+++
@@ -1,9 +0,0 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-Y2V5THG16Y"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-Y2V5THG16Y');
-</script>
